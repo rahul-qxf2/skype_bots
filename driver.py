bb03541508f64af433ac89211124e48a00995387
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-01-08 19:12:38 +0530

    driver file

--- driver.py
+++ driver.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 import re
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-
-
-
-
+import skype_credentials as credentials
 
 
 class Login():
@@ -14,37 +14,47 @@ class Login():
     def setup(self):
 
         try:
-            self.driver=webdriver.Chrome()
-            self.driver.delete_all_cookies()
-            self.driver.get('https://web.skype.com/en/')
-            self.driver.implicitly_wait(5)
-            self.driver.maximize_window()
+            email = credentials.USERNAME
+            password = credentials.PASSWORD
+            options = Options()
+            options.add_argument("start-maximized")
+            options.add_argument("disable-infobars")
+            options.add_argument("--disable-extensions")
+            self.driver = webdriver.Chrome(chrome_options=options)
+            self.driver.get('https://www.skype.com/en/')
+            print("Page Title is : %s" % self.driver.title)
+            linkelement= self.driver.find_element_by_xpath(
+                "//a[@class='btn secondaryCta small usernameBtn notAuthenticated']").click()
+            element =self.driver.find_element_by_xpath(
+                "//div[contains(@id, 'nav-buttons-wrapper')]//a[@role='menuitem' and contains(text(),'My account')]").click()
             self.driver.implicitly_wait(10)
-            is_displayed=self.driver.find_element_by_xpath(
-                "//span[@class='title x-hidden-focus' and contains(text(),'Sign in')]").click()
-            if is_displayed.is_displayed():
-               print("Element displayed")
-            result_flag=self.driver.find_element_by_xpath("//form[@id='i0281']")
-            if result_flag.is_displayed():
-               print("Moved to next tab")
-            self.driver.switch_to_frame(result_flag)
-            self.driver.find_element_by_id("i0116").send_keys("rahul.bhave81@outlook.com")
-            self.driver.find_element_by_xpath("idSIButton9").click()
-
+            userelement =self.driver.find_element_by_xpath(
+                "//input[@class='form-control ltr_override' and @name='loginfmt']")
+            userelement.click()
+            userelement.clear()
+            userelement.send_keys(email)
+            self.driver.find_element_by_xpath("//input[@class='btn btn-block btn-primary' and @id='idSIButton9']").click()
+            self.driver.find_element_by_xpath("//input[@name='passwd' and @type='password']").send_keys(password)
+            self.driver.implicitly_wait(10)
+            wait= WebDriverWait(self.driver,10)
+            waiting = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@class='btn btn-block btn-primary' and @type='submit']")))
+            signinelement=self.driver.find_element_by_xpath("//input[@class='btn btn-block btn-primary' and @type='submit']")
+            signinelement.click()
+           
+            
+            
+         
         except Exception as e:
 
             print(e)
 
+    
     def teardown(self):
 
         self.driver.quit()
-    
-    
-
 
 
 if __name__ == "__main__":
 
-    login=Login()
+    login = Login()
     login.setup()
-    login.teardown()
