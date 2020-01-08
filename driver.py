6db06f501745d4bd0627a886c6dd9bceb470c16e
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-01-08 21:37:54 +0530

    added code for the headless mode

--- driver.py
+++ driver.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import re
 import time
 import skype_credentials as credentials
@@ -17,39 +18,63 @@ class Login():
             email = credentials.USERNAME
             password = credentials.PASSWORD
             options = Options()
+            options.add_argument("--headless")
+            options.add_argument('--no-sandbox')
             options.add_argument("start-maximized")
             options.add_argument("disable-infobars")
             options.add_argument("--disable-extensions")
+            options.add_argument('--ignore-certificate-errors')
             self.driver = webdriver.Chrome(options=options)
+            options.headless = True
             self.driver.get('https://www.skype.com/en/')
+            print("Headless Chrome Initialized")
             print("Page Title is : %s" % self.driver.title)
-            linkelement= self.driver.find_element_by_xpath(
+
+            wait = WebDriverWait(self.driver, 10)
+            '''waiting = wait.until(EC.element_to_be_clickable(
+                (By.XPATH, "//a[@class='btn secondaryCta small usernameBtn notAuthenticated']")))'''
+            linkelement = self.driver.find_element_by_xpath(
                 "//a[@class='btn secondaryCta small usernameBtn notAuthenticated']").click()
-            element =self.driver.find_element_by_xpath(
+            print("Link accessed")
+
+            wait = WebDriverWait(self.driver, 10)
+            waiting = wait.until(EC.element_to_be_clickable(
+                (By.XPATH, "//div[contains(@id, 'nav-buttons-wrapper')]//a[@role='menuitem' and contains(text(),'My account')]")))
+            element = self.driver.find_element_by_xpath(
                 "//div[contains(@id, 'nav-buttons-wrapper')]//a[@role='menuitem' and contains(text(),'My account')]").click()
+            print("My account clicked")
+
             self.driver.implicitly_wait(10)
-            userelement =self.driver.find_element_by_xpath(
+            userelement = self.driver.find_element_by_xpath(
                 "//input[@class='form-control ltr_override' and @name='loginfmt']")
+            print("Login form opened")
+
             userelement.click()
             userelement.clear()
             userelement.send_keys(email)
-            self.driver.find_element_by_xpath("//input[@class='btn btn-block btn-primary' and @id='idSIButton9']").click()
-            self.driver.find_element_by_xpath("//input[@name='passwd' and @type='password']").send_keys(password)
+            print("Username entered")
+
+            self.driver.find_element_by_xpath(
+                "//input[@class='btn btn-block btn-primary' and @id='idSIButton9']").click()
+            print("Next")
+
+            self.driver.find_element_by_xpath(
+                "//input[@name='passwd' and @type='password']").send_keys(password)
+            print("Password entered")
+
             self.driver.implicitly_wait(10)
-            wait= WebDriverWait(self.driver,10)
-            waiting = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@class='btn btn-block btn-primary' and @type='submit']")))
-            signinelement=self.driver.find_element_by_xpath("//input[@class='btn btn-block btn-primary' and @type='submit']")
+            wait = WebDriverWait(self.driver, 10)
+            waiting = wait.until(EC.element_to_be_clickable(
+                (By.XPATH, "//input[@class='btn btn-block btn-primary' and @type='submit']")))
+            signinelement = self.driver.find_element_by_xpath(
+                "//input[@class='btn btn-block btn-primary' and @type='submit']")
             signinelement.click()
             print("Page Title is : %s" % self.driver.title)
-           
-            
-            
-         
+
         except Exception as e:
 
             print(e)
 
-    
     def teardown(self):
 
         self.driver.quit()
